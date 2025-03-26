82c68d4ff705bcb8d675f3b7e8efc14d16c15595
justyce-bellevue <jjbonczynski@my365.bellevue.edu>
2025-03-26 17:35:11 -0500

    Broken code for module 3 discussion

---
+++ Module3Discussion.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+
+    if (number > 0 {
+        cout << "The number is positive." << endl;
+    }
+    else {
+        cout << "The number is non-positive." << endl;
+    }
+
+    if (result == 1) {
+        cout << "Result is 1." << endl;
+    }
+    
+    return 0;
+}

726bb2d7497be144301c878f3a506df69a86140c
justyce-bellevue <jjbonczynski@my365.bellevue.edu>
2025-04-07 18:20:11 -0500

    Update M4Assignment.cpp

--- M4Assignment.cpp
+++ M4Assignment.cpp
@@ -2,21 +2,21 @@
 using namespace std;
 
 int main() {
-    int a, b, t;
+    int firstDish, additionalTime, totalTime;
 
     cout << "Enter the time, in minutes, to prepare the first dish: ";
-    cin >> a;
+    cin >> firstDish;
     cout << "Enter the additional time each dish will take to make: ";
-    cin >> b;
+    cin >> additionalTime;
     cout << "Enter the total time available for cooking: ";
-    cin >> t;
+    cin >> totalTime;
 
     int dishes = 0;
     int timeSpent = 0;
 
     while (true) {
-        int dishTime = a + b * dishes;
-        if (timeSpent + dishTime > t) {
+        int dishTime = firstDish + additionalTime * dishes;
+        if (timeSpent + dishTime > totalTime) {
             break;
         }
         timeSpent += dishTime;
